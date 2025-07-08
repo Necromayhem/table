94bb23cb8818de9cd522864bdd279307e74cc6b9
Alex <alex@example.com>
2025-07-08 19:30:24 +0300

    fix

--- src/components/MenuBar.vue
+++ src/components/MenuBar.vue
@@ -1,4 +1,4 @@
-<script setup>
+<script setup lang="ts">
 import { ref, computed } from 'vue'
 import { useRouter, useRoute } from 'vue-router'
 import { Menubar } from 'primevue'
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,20 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import UsersPage from '../views/UsersPage.vue'
+import FavoritesPage from '../views/FavoritesPage.vue'
 
 const router = createRouter({
 	history: createWebHistory(),
 	routes: [
-		{
-			path: '/users',
-			component: () => import('../views/UsersPage.vue'),
-		},
-		{
-			path: '/favorites',
-			component: () => import('../views/FavoritesPage.vue'),
-		},
-		{
-			path: '/',
-			redirect: '/users',
-		},
+		{ path: '/users', component: UsersPage },
+		{ path: '/favorites', component: FavoritesPage },
+		{ path: '/', redirect: '/users' },
 	],
 })
 
